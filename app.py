2fe00a314ebc4d420ac619b7049cfb3b4d82d329
Attila Z Gajdos <azgajdos@gmail.com>
2023-10-01 01:30:19 +0100

    new app created

---
+++ app.py
@@ -0,0 +1,101 @@
+from flask import Flask, render_template, jsonify
+from datetime import datetime  # Import the datetime module
+import requests
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/sites')
+def sites():
+    # Fetch weather data from the API
+    api_url = "https://api.openweathermap.org/data/2.5/forecast?lat=55.59194&lon=-3.66288&units=metric&appid=69129460125ea343f55bb51da80ec507"
+    response = requests.get(api_url)
+    if response.status_code == 200:
+        data = response.json()
+        
+        # Process the data and create the forecast
+        forecast_data = data['list'][:4 * 8]
+        forecast = []
+        
+        for item in forecast_data:
+            date = item['dt']
+            time_str = datetime.utcfromtimestamp(date).strftime('%H:%M')
+            
+            # Check if the time is between 09:00 and 21:00
+            if '09:00' <= time_str <= '21:00':
+                day_of_week = datetime.utcfromtimestamp(date).strftime('%a')
+                date_str = datetime.utcfromtimestamp(date).strftime('%d/%m/%Y')
+                temperature = round(item['main']['temp'])
+                description = item['weather'][0]['description'].capitalize()
+                wind_direction = get_wind_direction(item['wind']['deg'])
+                wind_speed = round(item['wind']['speed'], 1)
+                rain = 'rain' in item and item['rain']
+                
+                # Determine fly condition based on JavaScript conditions
+                fly_condition, fly_condition_class = get_fly_condition(wind_direction, description, wind_speed)
+                
+                forecast.append({
+                    'dayOfWeek': day_of_week,
+                    'date': date_str,
+                    'time': time_str,
+                    'temperature': temperature,
+                    'description': description,
+                    'windDirection': wind_direction,
+                    'windSpeed': wind_speed,
+                    'rain': rain,
+                    'flyCondition': fly_condition,
+                    'flyConditionClass': fly_condition_class  # Add fly condition CSS class
+                })
+
+        # Render the HTML template with the forecast data
+        return render_template('sites.html', forecast=forecast)
+    else:
+        return "Error fetching weather data"
+
+# Helper function to get wind direction
+def get_wind_direction(degree):
+    directions = ["N", "NNE", "NE", "ENE", "E", "ESE", "SE", "SSE", "S", "SSW", "SW", "WSW", "W", "WNW", "NW", "NNW"]
+    return directions[round((degree % 360) / 22.5)]
+
+# Helper function to determine fly condition based on JavaScript conditions
+def get_fly_condition(wind_direction, description, wind_speed):
+    if (
+        wind_direction in ["S", "SSW", "SSE", "SW", "SE"]
+        and "rain" in description.lower()
+    ):
+        return "Not flyable due to the rainy conditions.", "rain"
+    elif (
+        wind_direction in ["S", "SSW", "SSE", "SW", "SE"]
+        and 0 <= wind_speed <= 3
+    ):
+        return "Ideal conditions for beginners and thermal flying", "ideal"
+    elif (
+        wind_direction in ["S", "SSW", "SSE", "SW", "SE"]
+        and 3 < wind_speed <= 5
+    ):
+        return "Good conditions for most pilots", "good"
+    elif (
+        wind_direction in ["S", "SSW", "SSE", "SW", "SE"]
+        and 5 < wind_speed <= 8
+    ):
+        return "Challenging conditions for intermediate pilots", "challenging"
+    elif (
+        wind_direction in ["S", "SSW", "SSE", "SW", "SE"]
+        and 8 < wind_speed <= 11
+    ):
+        return "Difficult conditions for experienced pilots only", "difficult"
+    elif (
+        wind_direction in ["S", "SSW", "SSE", "SW", "SE"]
+        and 11 < wind_speed <= 15
+    ):
+        return "Dangerous conditions for all pilots", "dangerous"
+    elif wind_direction in ["S", "SSW", "SSE", "SW", "SE"] and wind_speed > 15:
+        return "Extreme conditions, not suitable for flying", "extreme"
+    else:
+        return "The wind direction is not suitable for this site.", "unsuitable"
+
+if __name__ == '__main__':
+    app.run()
---
+++ static/script.js
@@ -0,0 +1,142 @@
+function deg2rad(deg) {
+  return deg * (Math.PI / 180.0);
+}
+
+function drawWindCompass(canvas, yellows, greens) {
+  if (yellows == null || greens == null) return;
+
+  let padding = 7;
+  let borderLineWidth = 3;
+  let textSize = 21;
+  let textStrokeSize = 1;
+  const compassPoints = ["S", "E", "N", "W"];
+  let r = Math.min(canvas.width, canvas.height) / 2 - textSize / 2;
+
+  let ctx = canvas.getContext("2d");
+  let center = [canvas.width / 2, canvas.height / 2];
+  // Yellows
+  ctx.fillStyle = "#e6e619";
+  yellows.forEach(function (zone) {
+    ctx.beginPath();
+    ctx.moveTo(center[0], center[1]);
+    ctx.arc(
+      center[0],
+      center[1],
+      r,
+      deg2rad(zone[0] - 90),
+      deg2rad(zone[1] - 90)
+    );
+    ctx.closePath();
+    ctx.fill();
+  });
+
+  // Greens
+  ctx.fillStyle = "#30cc1f";
+  greens.forEach(function (zone) {
+    ctx.beginPath();
+    ctx.moveTo(center[0], center[1]);
+    ctx.arc(
+      center[0],
+      center[1],
+      r,
+      deg2rad(zone[0] - 90),
+      deg2rad(zone[1] - 90)
+    );
+    ctx.closePath();
+    ctx.fill();
+  });
+
+  // Outer Rim
+  ctx.strokeStyle = "#000";
+  ctx.lineWidth = borderLineWidth;
+  ctx.beginPath();
+  ctx.arc(center[0], center[1], r, 0, Math.PI * 2);
+  ctx.closePath();
+  ctx.stroke();
+
+  // Points of the Compass
+  ctx.strokeStyle = "#000";
+  ctx.fillStyle = "#3ff";
+  ctx.font = "bold " + textSize + "px Arial";
+  ctx.lineWidth = textStrokeSize;
+  compassPoints.forEach(function (pt, idx) {
+    const th = deg2rad(idx * 90);
+    const x = center[0] + Math.sin(th) * r;
+    const y = center[1] + Math.cos(th) * r;
+
+    ctx.fillText(pt, x - textSize / 3, y + textSize / 3);
+    ctx.strokeText(pt, x - textSize / 3, y + textSize / 3);
+  });
+}
+
+const markerColors = ["blue", "green", "gold", "violet", "grey", "orange"];
+let icons = [];
+markerColors.forEach(function (color) {
+  icons.push(
+    new L.Icon({
+      iconUrl: `https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-${color}.png`,
+      shadowUrl:
+        "https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png",
+      iconSize: [25, 41],
+      iconAnchor: [12, 41],
+      popupAnchor: [1, -34],
+      shadowSize: [41, 41],
+    })
+  );
+});
+
+let redMarker = new L.Icon({
+  iconUrl: `https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png`,
+  shadowUrl:
+    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png",
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41],
+});
+
+$(function () {
+  let maps = [];
+
+  $(".accordion-collapse").on("shown.bs.collapse", function () {
+    let mapDOM = $(this).find(".placeMap").first();
+    let mapId = mapDOM.attr("id");
+    if (maps.hasOwnProperty(mapId)) return;
+    let map = L.map(mapId).setView(
+      [mapDOM.data("lat"), mapDOM.data("lon")],
+      13
+    );
+    L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      maxZoom: 19,
+      attribution:
+        '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    }).addTo(map);
+
+    let markers = [];
+    markers.push(
+      L.marker([mapDOM.data("lat"), mapDOM.data("lon")], {
+        icon: redMarker,
+      }).addTo(map)
+    );
+
+    mapDOM.data("subplaces").forEach(function (subplace, idx) {
+      markers.push(
+        L.marker([subplace.lat, subplace.lon], {
+          icon: icons[idx],
+          title: subplace.name,
+        }).addTo(map)
+      );
+    });
+
+    let group = new L.featureGroup(markers);
+    map.fitBounds(group.getBounds().pad(0.5));
+  });
+
+  $(".compass-canvas").each(function () {
+    drawWindCompass(
+      $(this)[0],
+      $(this).data("yellows"),
+      $(this).data("greens")
+    );
+  });
+});
